2174dc5b0f95c14169cf32d974c29b29ed4ef653
dmytro mykhalchuk <dimykhalchuk@yuriimykhals-MacBook-Pro.local>
2026-04-05 15:32:26 +0300

    Виправлено main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -5,9 +5,9 @@ terraform {
       version = "~> 2.34"
     }
   }
-  # Збереження tfstate файлу у хмарі (як вимагається в завданні)
+  # Збереження tfstate файлу у хмарі
   backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
+    endpoints                   = { s3 = "https://fra1.digitaloceanspaces.com" }
     region                      = "us-east-1"
     bucket                      = "mykhalchuk-tfstate"
     key                         = "task1/terraform.tfstate"
@@ -28,16 +28,16 @@ provider "digitalocean" {
 # VPC (Мережа)
 resource "digitalocean_vpc" "mykhalchuk_vpc" {
   name     = "mykhalchuk-vpc"
-  region   = "fra1"           # Найближчий до України регіон [cite: 7]
-  ip_range = "10.10.10.0/24"  # [cite: 8]
+  region   = "fra1"
+  ip_range = "10.10.10.0/24"
 }
 
 # ВМ (Droplet)
 resource "digitalocean_droplet" "mykhalchuk_node" {
   name     = "mykhalchuk-node"
-  image    = "ubuntu-24-04-x64" # Образ ОС: Ubuntu 24 [cite: 16]
-  region   = "fra1"             # Ідентичний до VPC [cite: 17]
-  size     = "s-2vcpu-4gb"      # Відповідає вимогам Minikube/Kubernetes [cite: 15]
+  image    = "ubuntu-24-04-x64"
+  region   = "fra1"
+  size     = "s-2vcpu-4gb"
   vpc_uuid = digitalocean_vpc.mykhalchuk_vpc.id
 }
 
@@ -46,7 +46,6 @@ resource "digitalocean_firewall" "mykhalchuk_firewall" {
   name = "mykhalchuk-firewall"
   droplet_ids = [digitalocean_droplet.mykhalchuk_node.id]
 
-  # Вхідні підключення [cite: 12]
   dynamic "inbound_rule" {
     for_each = ["22", "80", "443", "8000", "8001", "8002", "8003"]
     content {
@@ -56,7 +55,6 @@ resource "digitalocean_firewall" "mykhalchuk_firewall" {
     }
   }
 
-  # Вихідні підключення [cite: 13]
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
@@ -72,80 +70,5 @@ resource "digitalocean_firewall" "mykhalchuk_firewall" {
 # Бакет
 resource "digitalocean_spaces_bucket" "mykhalchuk_bucket" {
   name   = "mykhalchuk-bucket"
-  region = "fra1" # Ідентичний до VPC [cite: 21]
-}terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.34"
-    }
-  }
-  # Збереження tfstate файлу у хмарі (як вимагається в завданні)
-  backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
-    region                      = "us-east-1"
-    bucket                      = "mykhalchuk-tfstate"
-    key                         = "task1/terraform.tfstate"
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_s3_checksum            = true
-  }
-}
-
-provider "digitalocean" {
-  token             = var.do_token
-  spaces_access_id  = var.spaces_access_id
-  spaces_secret_key = var.spaces_secret_key
-}
-
-# VPC (Мережа)
-resource "digitalocean_vpc" "mykhalchuk_vpc" {
-  name     = "mykhalchuk-vpc"
-  region   = "fra1"           # Найближчий до України регіон [cite: 7]
-  ip_range = "10.10.10.0/24"  # [cite: 8]
-}
-
-# ВМ (Droplet)
-resource "digitalocean_droplet" "mykhalchuk_node" {
-  name     = "mykhalchuk-node"
-  image    = "ubuntu-24-04-x64" # Образ ОС: Ubuntu 24 [cite: 16]
-  region   = "fra1"             # Ідентичний до VPC [cite: 17]
-  size     = "s-2vcpu-4gb"      # Відповідає вимогам Minikube/Kubernetes [cite: 15]
-  vpc_uuid = digitalocean_vpc.mykhalchuk_vpc.id
-}
-
-# Фаєрвол
-resource "digitalocean_firewall" "mykhalchuk_firewall" {
-  name = "mykhalchuk-firewall"
-  droplet_ids = [digitalocean_droplet.mykhalchuk_node.id]
-
-  # Вхідні підключення [cite: 12]
-  dynamic "inbound_rule" {
-    for_each = ["22", "80", "443", "8000", "8001", "8002", "8003"]
-    content {
-      protocol         = "tcp"
-      port_range       = inbound_rule.value
-      source_addresses = ["0.0.0.0/0", "::/0"]
-    }
-  }
-
-  # Вихідні підключення [cite: 13]
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-  outbound_rule {
-    protocol              = "udp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-}
-
-# Бакет
-resource "digitalocean_spaces_bucket" "mykhalchuk_bucket" {
-  name   = "mykhalchuk-bucket"
-  region = "fra1" # Ідентичний до VPC [cite: 21]
+  region = "fra1"
 }
