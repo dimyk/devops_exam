e308ff63ee86e4a75520a16869161335b2ee7596
dmytro mykhalchuk <dimykhalchuk@yuriimykhals-MacBook-Pro.local>
2026-04-05 15:08:10 +0300

    Додано завдання 1

---
+++ task1/variables.tf
@@ -0,0 +1,14 @@
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+}
+
+variable "spaces_access_id" {
+  description = "Spaces Access Key"
+  type        = string
+}
+
+variable "spaces_secret_key" {
+  description = "Spaces Secret Key"
+  type        = string
+}
